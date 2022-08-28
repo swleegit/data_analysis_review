7b04197adb1db7fa6361a1ec6d6e6cc1ecaa3ee0
SungwooLEE <swlee@tukorea.ac.kr>
2022-08-28 15:27:11 +0900

    pip_install 복습

---
+++ pipinstall.py
@@ -0,0 +1,11 @@
+#파이썬에서 기본적으로 제공하는 패키지와 모듈외에 다른 사람이 이미 개발한 것들이 있음
+#처음부터 끝까지 코딩하는게 아니라 다른사람이 만든 것을 잘 활용하는 것도 능력임
+#https://pypi.org/에서 browse project에서 분류보고 선택사용
+#https://pypi.org/project/beautifulsoup4/ 웹스크래핑 패키지
+#pip와 conda의 차이점
+#파이썬 인터렉티브쉘에 pip 주소복사
+#pip list : 설치된 패키지(모듈) 전부 나열 
+#pip show <설치된 패키지 이름> : 해당 패키지 정보 알려줌 
+#pip install --upgrade 패키지명 :설치된 패키지 업그레이드
+#pip uninstall 패키지명 : 삭제 
+
